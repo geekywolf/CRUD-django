7c2e1125506c5597c2b2f87e967fd7346ff72020
Heretic <t4riumph@gmail.com>
2022-06-27 10:44:58 +0100

    deleted secretkey 'hider'

--- I4G034216BR3/I4G034216BR3/settings.py
+++ I4G034216BR3/I4G034216BR3/settings.py
@@ -21,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-j1lpez)nz3=ap#o+bcp=7#snif3al-%1!r33*o)3#v(vo0$)#!'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
