0dbdb50b6d6ebc1526e8a6f12a784e7f7a80f65f
Heretic <t4riumph@gmail.com>
2022-07-01 15:43:26 +0100

    created urls and views

--- I4G034216BR3/Blog/views.py
+++ I4G034216BR3/Blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 
+from Blog.models import Post
+from .models import Post
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy('Blog:all') 
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy('Blog:all') 
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy('Blog:all') 
